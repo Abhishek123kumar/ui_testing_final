957c250440aa9a2e548ed7ed48f30dc016ac2092
LAKSHYA <llakshya63@gmail.com>
2023-03-28 18:52:31 +0530

    some changes in router

--- src/Pages/History.js
+++ src/Pages/History.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from '../Components/Navbar'
 import HistoryCon from '../Components/HistoryCon'
 
 
 function History() {
+
+  useEffect(()=>{
+    if(localStorage.getItem("token")===null)
+    {
+      navigate("/");
+    }
+  },[])
+
   return (
     <div>
       <Navbar/>
--- src/Pages/MainHome.js
+++ src/Pages/MainHome.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from '../Components/Navbar'
 import Main from '../Components/Main'
 
 
 function MainHome() {
+
+    useEffect(()=>{
+        if(localStorage.getItem("token")===null)
+        {
+          navigate("/");
+        }
+      },[])
+
     return (
         <div>
             <Navbar />
